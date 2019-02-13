aa2c01e53622c4c15efcd9bad50857fe4e6e27e2
Armand Giraud <armand.giraud.ag@gmail.com>
2019-02-13 12:29:04 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quest2keys',
+      version='0.1',
+      description='transform question to keywords',
+      url='http://github.com/',
+      author='Armand Giraud',
+      author_email='armand.giraud.ag@gmail.com',
+      license='MIT',
+      packages=['quest2keys',"tests"],
+      zip_safe=False)
