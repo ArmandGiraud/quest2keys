46b097bfe3fe2068425bb2006b8826ff179311e9
Armand Giraud <armand.giraud.ag@gmail.com>
2019-02-13 12:32:43 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Quest2keys
+**a simple question 2 keywords in french based on pos-tags**
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='quest2keys',
       version='0.1',
       description='transform question to keywords',
-      url='http://github.com/',
+      url='https://github.com/ArmandGiraud/quest2keys',
       author='Armand Giraud',
       author_email='armand.giraud.ag@gmail.com',
       license='MIT',
